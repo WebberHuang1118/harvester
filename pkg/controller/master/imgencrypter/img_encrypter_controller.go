1a82b486cf3c450aaf7850d4fb374cd3d6da31b0
Webber Huang <webber.huang@suse.com>
2024-02-02 22:12:28 +0800

    Create transfer job

--- pkg/controller/master/imgencrypter/img_encrypter_controller.go
+++ pkg/controller/master/imgencrypter/img_encrypter_controller.go
@@ -3,24 +3,41 @@ package imgencrypter
 import (
 	"fmt"
 
+	catalogv1 "github.com/rancher/rancher/pkg/generated/controllers/catalog.cattle.io/v1"
+	"github.com/rancher/wrangler/pkg/condition"
+	ctlbatchv1 "github.com/rancher/wrangler/pkg/generated/controllers/batch/v1"
 	ctlcorev1 "github.com/rancher/wrangler/pkg/generated/controllers/core/v1"
 	ctlstoragev1 "github.com/rancher/wrangler/pkg/generated/controllers/storage/v1"
 	"github.com/sirupsen/logrus"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
 
 	harvesterv1 "github.com/harvester/harvester/pkg/apis/harvesterhci.io/v1beta1"
 	ctlharvesterv1 "github.com/harvester/harvester/pkg/generated/controllers/harvesterhci.io/v1beta1"
+	utilCatalog "github.com/harvester/harvester/pkg/util/catalog"
 )
 
 const (
-	cryptoSC   = "longhorn-migratable"
-	srcPVCName = "src-pvc"
-	srcPVCSize = "5Gi"
-	dstPVCName = "dst-pvc"
-	dstPVCSize = "6Gi"
+	cryptoSC                = "longhorn-migratable"
+	srcPVCName              = "src-pvc"
+	srcPVCSize              = "5Gi"
+	srcVolName              = "src-vol"
+	srcVolMntPath           = "/dev/src-vol"
+	dstPVCName              = "dst-pvc"
+	dstPVCSize              = "6Gi"
+	dstVolName              = "dst-vol"
+	dstVolMntPath           = "/dev/dst-vol"
+	transferJob             = "transfer-job"
+	releaseAppHarvesterName = "harvester"
+)
+
+var (
+	ConditionJobComplete = condition.Cond(batchv1.JobComplete)
+	ConditionJobFailed   = condition.Cond(batchv1.JobFailed)
 )
 
 type imgEncrypterHandler struct {
@@ -30,6 +47,10 @@ type imgEncrypterHandler struct {
 	pvcCache            ctlcorev1.PersistentVolumeClaimCache
 	imageCache          ctlharvesterv1.VirtualMachineImageCache
 	storageClassCache   ctlstoragev1.StorageClassCache
+	namespace           string
+	appCache            catalogv1.AppCache
+	jobs                ctlbatchv1.JobClient
+	jobCache            ctlbatchv1.JobCache
 }
 
 func (h *imgEncrypterHandler) createSrcPVC(encrypter *harvesterv1.ImgEncrypter) (*corev1.PersistentVolumeClaim, error) {
@@ -134,6 +155,89 @@ func (h *imgEncrypterHandler) checkDstPVC(encrypter *harvesterv1.ImgEncrypter) (
 	return dstPVC, nil
 }
 
+func (h *imgEncrypterHandler) createJob(encrypter *harvesterv1.ImgEncrypter, srcPVC string, dstPVC string) (*batchv1.Job, error) {
+	jobImage, err := utilCatalog.FetchAppChartImage(h.appCache, h.namespace, releaseAppHarvesterName, []string{"generalJob", "image"})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get harvester image (%s): %v", jobImage.ImageName(), err)
+	}
+
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      transferJob,
+			Namespace: encrypter.Namespace,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					RestartPolicy: corev1.RestartPolicyNever,
+					// Volumes: []corev1.Volume{{
+					// 	Name: srcVolName,
+					// 	VolumeSource: corev1.VolumeSource{
+					// 		PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					// 			ClaimName: srcPVC,
+					// 		},
+					// 	},
+					// }, {
+					// 	Name: dstVolName,
+					// 	VolumeSource: corev1.VolumeSource{
+					// 		PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					// 			ClaimName: dstPVC,
+					// 		},
+					// 	},
+					// }},
+					ServiceAccountName: "harvester",
+				},
+			},
+		},
+	}
+	podTemplate := &job.Spec.Template
+
+	podTemplate.Spec.Containers = []corev1.Container{
+		{
+			Name:      transferJob,
+			Image:     jobImage.ImageName(),
+			Command:   []string{"sleep"},
+			Args:      []string{"7200"},
+			Resources: corev1.ResourceRequirements{},
+			// VolumeMounts: []corev1.VolumeMount{
+			// 	{Name: srcVolName, MountPath: srcVolMntPath},
+			// 	{Name: dstVolName, MountPath: dstVolMntPath},
+			// },
+			ImagePullPolicy: corev1.PullIfNotPresent,
+			SecurityContext: &corev1.SecurityContext{
+				Privileged: pointer.Bool(true),
+			},
+		},
+	}
+
+	return h.jobs.Create(job)
+}
+
+func (h *imgEncrypterHandler) checkJob(encrypter *harvesterv1.ImgEncrypter, srcPVC string, dstPVC string) (*batchv1.Job, error) {
+	job, err := h.jobCache.Get(encrypter.Namespace, transferJob)
+	if apierrors.IsNotFound(err) {
+		if _, err := h.createJob(encrypter, srcPVC, dstPVC); err != nil {
+			logrus.Infof("checkJob: createJob fail with err %v", err)
+			return nil, err
+		}
+		return nil, fmt.Errorf("checking transfer job again ")
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if ConditionJobFailed.IsTrue(job) {
+		return nil, fmt.Errorf("transfer job failed")
+	}
+
+	if !ConditionJobComplete.IsTrue(job) {
+		return nil, fmt.Errorf("transfer job not complete yet")
+	}
+
+	return job, nil
+}
+
 func (h *imgEncrypterHandler) OnChanged(_ string, encrypter *harvesterv1.ImgEncrypter) (*harvesterv1.ImgEncrypter, error) {
 	if encrypter == nil || encrypter.DeletionTimestamp != nil {
 		return encrypter, nil
@@ -154,17 +258,23 @@ func (h *imgEncrypterHandler) OnChanged(_ string, encrypter *harvesterv1.ImgEncr
 	logrus.Infof("reconcile imgEncrypter %v for img %s/%s", encrypter.Name,
 		encrypter.Spec.SrcImgNamespace, encrypter.Spec.SrcImgName)
 
-	_, err := h.checkSrcPVC(encrypter)
+	srcPVC, err := h.checkSrcPVC(encrypter)
 	if err != nil {
 		return nil, err
 	}
 	stage = 1
 
-	_, err = h.checkDstPVC(encrypter)
+	dstPVC, err := h.checkDstPVC(encrypter)
 	if err != nil {
 		return nil, err
 	}
 	stage = 2
+
+	_, err = h.checkJob(encrypter, srcPVC.Name, dstPVC.Name)
+	if err != nil {
+		return nil, err
+	}
+	stage = 3
 
 	return encrypter, nil
 }
--- pkg/controller/master/imgencrypter/register.go
+++ pkg/controller/master/imgencrypter/register.go
@@ -10,11 +10,13 @@ const (
 	imgEncrypterControllerName = "img-encrypter-controller"
 )
 
-func Register(ctx context.Context, management *config.Management, _ config.Options) error {
+func Register(ctx context.Context, management *config.Management, options config.Options) error {
 	encrypters := management.HarvesterFactory.Harvesterhci().V1beta1().ImgEncrypter()
 	pvcs := management.CoreFactory.Core().V1().PersistentVolumeClaim()
 	images := management.HarvesterFactory.Harvesterhci().V1beta1().VirtualMachineImage()
 	storageClasses := management.StorageFactory.Storage().V1().StorageClass()
+	appCache := management.CatalogFactory.Catalog().V1().App().Cache()
+	jobs := management.BatchFactory.Batch().V1().Job()
 
 	imgEncrypterHandler := &imgEncrypterHandler{
 		encrypterController: encrypters,
@@ -23,6 +25,10 @@ func Register(ctx context.Context, management *config.Management, _ config.Optio
 		pvcCache:            pvcs.Cache(),
 		imageCache:          images.Cache(),
 		storageClassCache:   storageClasses.Cache(),
+		namespace:           options.Namespace,
+		appCache:            appCache,
+		jobs:                jobs,
+		jobCache:            jobs.Cache(),
 	}
 
 	encrypters.OnChange(ctx, imgEncrypterControllerName, imgEncrypterHandler.OnChanged)
